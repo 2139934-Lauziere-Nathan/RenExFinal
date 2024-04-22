de33a0b8a83ef6ad1013172b70ed3d7f966bc5a9
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-04-22 18:12:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pg = require('pg');
 const app = express();
-const PORT = 3000;
+//const PORT = 3000;
 app.use(express.json());
 app.get('/', (req, res) => {
     res.send("<p>index</p>");
