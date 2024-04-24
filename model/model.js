33c76609ac0632ba3bb14a76b649d2352ec28151
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2024-04-24 14:32:08 -0400

    update

--- model/model.js
+++ model/model.js
@@ -4,7 +4,7 @@ const db = require("../.src/config/pg_db");
 const mod = {
     getAll: (utilisateur_id) => {
         return new Promise((resolve, reject) => {
-            const query = `SELECT * FROM public.taches WHERE utilisateur_id = $1`;
+            const query = 'SELECT * FROM public.taches WHERE utilisateur_id = $1';
             const value = [utilisateur_id]
             db.query(query, value, (err, result) => {
                 if (err) {
